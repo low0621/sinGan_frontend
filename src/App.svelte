f31a8f2da03d7d0a6e2849990199e460bc845eb7
low0621 <low0621@gmail.com>
2021-11-14 23:20:21 +0800

    chore: change training complete condition

--- src/App.svelte
+++ src/App.svelte
@@ -54,21 +54,20 @@ onMount(async () => {
 .h-screen.w-screen.flex.items-center.justify-center
   .flex.flex-col(class='w-3/6').items-center
     .mb-4.flex.flex-col.justify-center.items-center.w-full
-      +if('progress != "100%"')
+      +if('progress != "done"')
         .relative.pt-1.w-full.text-center {progress}
-          .overflow-hidden.h-2.mb-4.text-xs.text-center.flex.rounded.bg-blue-200
+          .overflow-hidden.h-2.mt-4.text-xs.text-center.flex.rounded.bg-blue-200
             .transition-width.duration-300.shadow-none.flex.flex-col.text-center.whitespace-nowrap.text-white.justify-center.bg-blue-500(style='width: {progress}')
         +else()
-          div Training complete!!
-    +if('progress')
-      .inline-flex.mb-4.justify-between.w-full
-        +each('modes as mode')
-          .inline-flex.items-center.mr-4
-            input.mr-1(type='radio' name='mode' on:click!='{() => currentMode = mode}' value='{mode}')
-            span {mode}
-      .flex.flex-col.w-full
-        svelte:component(this='{components[currentMode]}' bind:value='{props}')
-      button.bg-blue-500.text-white.font-bold.py-2.px-4(class='w-28 hover:bg-blue-700 rounded' on:click='{submit}') Submit
+          .mb-4 Training complete!!
+          .inline-flex.mb-4.justify-between.w-full.flex-wrap
+            +each('modes as mode')
+              .inline-flex.items-center.mr-4(class='last:mr-0')
+                input.mr-1(type='radio' name='mode' on:click!='{() => currentMode = mode}' value='{mode}')
+                span {mode}
+          .flex.flex-col.w-full
+            svelte:component(this='{components[currentMode]}' bind:value='{props}')
+          button.bg-blue-500.text-white.font-bold.py-2.px-4(class='w-28 hover:bg-blue-700 rounded' on:click='{submit}') Submit
 </template>
 
 <style>
