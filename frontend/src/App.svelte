21b3579864a4ee821b28c6e95ce01947e87471c9
low0621 <low0621@gmail.com>
2021-12-21 17:33:43 +0800

    feat: add randomsample

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -10,20 +10,25 @@ let components = {
   'Editing': SourceTargetMask,
   'Harmonization': SourceTargetMask,
   'SuperResolution': Source,
-  'Animation': Source
+  'Animation': Source,
+  'RandomSample': Source
 }
 let currentMode = 'Paint2Image'
 let maxScale = 99
-let modes = ['Paint2Image', 'Editing', 'Harmonization', 'SuperResolution', 'Animation']
+let modes = ['Paint2Image', 'Editing', 'Harmonization', 'SuperResolution', 'Animation','RandomSample']
 let progress = '100%'
 let source, target, mask, scale
-let _status, props
+let _status, props, width, height
 
 
 const submit = () => {
   switch (currentMode) {
     case 'SuperResolution':
       props['resolution'] = scale
+      break
+    case 'RandomSample':
+      props['width'] = width
+      props['height'] = height
       break
     case 'Anmation':
       break
@@ -110,7 +115,7 @@ onMount(async () => {
 
 <template lang='pug'>
 .w-screen.h-screen.bg-white
-  .h-full.box-border.pt-10.pb-10.mx-auto(class='w-10/12')
+  .h-full.box-border.py-10.mx-auto(class='w-10/12')
     .mb-8
       .text-4xl.mb-4.eras.text-blue SinGAN
       div.w-full
@@ -124,9 +129,16 @@ onMount(async () => {
             +each('modes as mode')
                 input.eras.rounded.appearance-none.cursor-pointer.border.border-blue.flex.justify-center.py-2(class='hover:bg-blue hover:text-white' class!='{currentMode == mode ? "bg-blue text-white" : "text-blue"}' type='radio' name='mode' on:click!='{() => currentMode = mode}' label='{mode}' checked='{currentMode == mode}')
           .flex.flex-col(class='w-1/2')
-            .flex.mb-8.justify-between.items-center(class!='{_status != "done" ? "opacity-30 pointer-events-none" : ""}')
-              .eras.text-blue Scale
-              input.eras.text-center.px-4.py-2.rounded-lg.border.border-blue.text-xs.w-20(type='number' class='focus:outline-none' min=1 max='{maxScale}' bind:value='{scale}')
+            .flex.flex-col.mb-8(class!='{_status != "done" ? "opacity-30 pointer-events-none" : ""}')
+              .flex.justify-between.mb-2
+                .eras.text-blue Scale
+                input.eras.text-center.px-4.py-2.rounded-lg.border.border-blue.text-xs.w-20(type='number' class='focus:outline-none' min=1 max='{maxScale}' bind:value='{scale}')
+              .flex.justify-between.mb-2
+                .eras.text-blue(class!='{currentMode != "RandomSample" ? "opacity-30 pointer-events-none" : ""}') Width
+                input.eras.text-center.px-4.py-2.rounded-lg.border.border-blue.text-xs.w-20(type='number' class='focus:outline-none' min=1 bind:value='{width}' class!='{currentMode != "RandomSample" ? "opacity-30 pointer-events-none" : ""}')
+              .flex.justify-between
+                .eras.text-blue(class!='{currentMode != "RandomSample" ? "opacity-30 pointer-events-none" : ""}') Height
+                input.eras.text-center.px-4.py-2.rounded-lg.border.border-blue.text-xs.w-20(type='number' class='focus:outline-none' min=1 bind:value='{height}' class!='{currentMode != "RandomSample" ? "opacity-30 pointer-events-none" : ""}')
             button.eras.bg-purple.text-white.font-bold.mb-8.py-2.rounded(class!='{_status != "done" ? "opacity-30 pointer-events-none" : ""}' on:click!='{submit}') Submit
             button.eras.bg-purple.text-white.font-bold.py-2.rounded(class!='{_status == "done" ? "opacity-30 pointer-events-none" : ""}'  on:click!='{stop}') Stop
       .flex.flex-col(class='w-8/12')
